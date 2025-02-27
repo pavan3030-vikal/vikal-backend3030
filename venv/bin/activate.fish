50594e119a594e226ab5c3aee6aa84b1b64dd59e
pavan3030 <pvangapa3@gitam.in>
2025-02-27 17:16:37 +0530

    new changes

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/karthikvangapandu/Desktop/VIKAL-4040/backend/venv"
+set -gx VIRTUAL_ENV "/Users/karthikvangapandu/Desktop/VIKAL-4040/vikal-backend/vikal-backend/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
