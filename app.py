2d7e1d5a3bf2ea2af6492ef2aeb6b14f946a30dd
pavan3030-vikal <pvangapa3@gitam.in>
2025-02-28 11:25:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "https://vikal-new-production.up.railway.app"}}, supports_credentials=True)
 
 OPENAI_API_URL = "https://api.openai.com/v1/chat/completions"
-OPENAI_API_KEY = "your_openai_api_key_here"  # Replace with your actual OpenAI API key
+OPENAI_API_KEY = "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"  # Replace with your actual OpenAI API key
 client = MongoClient("mongodb://mongo:vEvIixiKtkFvKHuMkvTfzjVfCjYbZhGF@shortline.proxy.rlwy.net:42954")
 db = client["vikal"]
 chat_history = db["chat_history"]
